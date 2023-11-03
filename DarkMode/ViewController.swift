194bcf79a49025f69d219ab0fee9c59fbff64e15
İsmail Harmanda <iharmanda@hotmail.com>
2023-11-04 01:18:11 +0300

    listen and handle dark mode changes

--- DarkMode/ViewController.swift
+++ DarkMode/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = .white
+        } else {
+            changeButton.tintColor = .black
+        }
     }
 
 
